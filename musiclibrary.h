689773d75a81bcb21541f965b973f23ec9bff742
kli24s <84247346+kli24s@users.noreply.github.com>
2021-10-24 19:41:42 -0400

    Add files via upload

--- musiclibrary.h
+++ musiclibrary.h
@@ -1,1 +1,19 @@
-//
+struct song_node ** make_library();
+
+struct song_node ** add_song(struct song_node **library, char *name, char *artist);
+
+struct song_node * search_song(struct song_node **library, char *name, char *artist);
+
+struct song_node * search_artist(struct song_node **s, char *artist);
+
+void print_letter(struct song_node **library, char letter);
+
+void print_artist(struct song_node **library, char *artist);
+
+void print_library(struct song_node **library);
+
+void shuffle(struct song_node **library);
+
+struct song_node ** delete_song(struct song_node **library, char *name, char *artist);
+
+struct song_node ** clear_library(struct song_node **library);
